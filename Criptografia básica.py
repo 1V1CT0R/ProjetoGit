10307bce22ac69f440e7dcd0aea297aec05fd75d
Victor <victorpedrozo5@gmail.com>
2022-01-13 16:53:23 -0300

    Inserindo o projeto de Criptografia

---
+++ Criptografia básica.py
@@ -0,0 +1,25 @@
+'''1)
+        Fazer um programa que codifique uma mensagem utilizando a cifra de César.
+        Em sequência fazer um programa que descodifique a mensagem.'''
+
+método_encriptar=1
+método_desencriptar=0
+
+def caesar(frase,posição,método):
+    alfabeto="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    nova_frase=""
+
+    for x in frase:
+        índice=alfabeto.find(x)
+        if índice == -1:
+            nova_frase+=x
+        else:
+            if método==1:
+                novo_índice = índice + posição
+            else:
+                novo_índice = índice - posição
+            novo_índice = novo_índice % len(alfabeto)
+            nova_frase+=alfabeto[novo_índice]
+    return nova_frase
+
+print(caesar("Olá Mundo!",3,1))
